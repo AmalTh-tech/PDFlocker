ffc1ec748b46d27c760737699407713fb62e1f21
AmalTh-tech <56636201+AmalTh-tech@users.noreply.github.com>
2020-11-01 18:41:24 +0530

    Add files via upload

---
+++ pdflocker.py
@@ -0,0 +1,49 @@
+from PyPDF2 import PdfFileWriter,PdfFileReader
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox as msg
+from tkinter import filedialog
+import time
+def browseFiles():
+    global filename
+    filename = filedialog.askopenfilename(initialdir = "/", 
+										title = "Select a File", 
+										filetypes = (("Text files", 
+														"*.txt*"), 
+													("all files", 
+														"*.*")))
+    ttk.Label(win,text = "You have chosen: "+filename).grid(column = 4,row = 5)
+def secure_pdf(file,password):
+    parser = PdfFileWriter()
+    pdf = PdfFileReader(file)
+
+    for page in range(pdf.numPages):
+        parser.addPage(pdf.getPage(page))
+    parser.encrypt(password)
+
+    with open(filename+"encrypted.pdf","wb") as f:
+        parser.write(f)
+        f.close()
+    print("encrypted file created....")
+
+def confirm():
+    secure_pdf(filename,passwd.get())
+    msg.showinfo("PDF locker says","Password protected file successfully created in the same folder as the original file.")
+    time.sleep(5)
+    win.destroy()
+
+if __name__ == "__main__":
+    win  = tk.Tk()
+    win.geometry("300x400")
+    win.title("PDF Locker")
+    ttk.Label(win,text = "Choose file location:").grid(column = 0,row = 0)
+    ttk.Button(win,text="Choose",command = browseFiles).grid(column = 1,row = 0)
+    passwd = tk.StringVar()
+    ttk.Label(win,text="Enter password:").grid(column=0,row=1)
+    passwd_entered = ttk.Entry(win,width = 10,textvariable = passwd)
+    passwd_entered.grid(column=1,row=1,padx = 2)
+
+    ttk.Button(win,text = "Confirm",command = confirm).grid(column=0,row = 3,pady = 3)
+    win.mainloop()
+    
+    
